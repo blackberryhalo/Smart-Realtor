784f4f7472e850592b57edf25c0fc7ad28d7b81a
blackberryhalo <752812820@qq.com>
2023-08-10 16:19:35 +0800

    add

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,91 @@
+<div class="container">
+    <div class="pt-20">
+        <h2 class="section-title text-4xl text-center pb-8 langBN">{{ __('Article & Advices') }}</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px; background-image: url(/img/6.jpg)" class="bg-cover bg-center"></div>
+                    {{-- <div style="min-width:130px; background-image: url(https://picsum.photos/1200/800)" class="bg-cover bg-center"></div> --}}
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">美股透視 - 美企業績參差及美債息重上4厘水平影響美股表現</h3>
+                        <a href="" class="fullwidth-btn">閱讀更多</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px; background-image: url(/img/3.jpg)" class="bg-cover bg-center"></div>
+                    {{-- <div style="min-width:130px; background-image: url(https://picsum.photos/1200/800)" class="bg-cover bg-center"></div> --}}
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">各行各業均可以辦理綠色貸款及可持續發展表現掛鈎貸款</h3>
+                        <a href="" class="fullwidth-btn">閱讀更多</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px; background-image: url(/img/6.jpg)" class="bg-cover bg-center"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">中銀香港資產管理有限公司: 2023年第三季度投資市場展望</h3>
+                        <a href="" class="fullwidth-btn">閱讀更多</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pt-20 text-lg border-b-2 border-gray-300 pb-16 mb-20">
+        <h2 class="section-title text-4xl text-center pb-8 langBN">{{ __('Additional services') }}</h2>
+        <div class="flex -mx-4" align="center">
+            <div class="flex-1 px-4">
+                <a href="" class="underline">最新消息</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">推廣優惠</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">市場資訊</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">企業資訊</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex pb-12">
+        <div class="w-1/4 pr-6" align="center">
+            <img width="100" src="/img/house-logo.png" alt="">
+            <p class="text-gray-500 mt-7 text-xs">© 2023 中國銀行(香港)有限公司。版權所有。</p>
+        </div>
+
+        <div class="w-3/4 font-bold leading-8">
+            <div class="flex -mx-6">
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="https://www.bochk.com/tc/importantnotice.html">重要聲明及私隱政策聲明</a></li>
+                        <li><a href="">貸款及信用卡產品資料概要</a></li>
+                        <li><a href="">監管披露</a></li>
+                    </ul>
+                </div>
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="https://www.bochk.com/tc/conditionsforservices.html">服務條款</a></li>
+                        <li><a href="">超連結政策</a></li>
+                        <li><a href="">保安資訊</a></li>
+                    </ul>
+                </div>
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="">一般銀行服務收費</a></li>
+                        <li><a href="">無障礙服務</a></li>
+                        <li><a href="">分行網絡</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<img class="w-full" src="/img/footer.jpg" alt="">
